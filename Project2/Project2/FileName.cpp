791c9c129e907f0b5c312de5894daf626acd3af5
ththfql <bona5141@naver.com>
2025-11-04 16:59:51 +0900

    finish

---
+++ Project2/Project2/FileName.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	int q, r;
+	printf("2개의 정수를 입력하세요(예: 9 2): ");
+	scanf("%d", &a, &b);
+
+	printf(("몫은 %d입니다. \n", q);
+	printf("나머지는 %d입니다. \n", r);
+
+	return 0;)
+
+
+}
